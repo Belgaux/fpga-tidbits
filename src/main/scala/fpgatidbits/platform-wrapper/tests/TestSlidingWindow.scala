01a3b6b57c1a492beb1a0e0fcbdd7f38579c6ace
finninde <finninde@stud.ntnu.no>
2017-10-24 20:08:23 +0200

    Add writer, calculate byte offset

--- src/main/scala/fpgatidbits/platform-wrapper/tests/TestSlidingWindow.scala
+++ src/main/scala/fpgatidbits/platform-wrapper/tests/TestSlidingWindow.scala
@@ -9,7 +9,7 @@ import fpgatidbits.rosetta._
 
 class TestSlidingWindow(p: PlatformWrapperParams, _wordSize:Int) extends GenericAccelerator(p) {
   val wordSize = _wordSize 
-  val numMemPorts = 1
+  val numMemPorts = 2
 
   val io = new GenericAcceleratorIF(numMemPorts, p){
     // Inputs
@@ -18,14 +18,20 @@ class TestSlidingWindow(p: PlatformWrapperParams, _wordSize:Int) extends Generic
     val numChannels = UInt(INPUT, width=32)
     val stride =      UInt(INPUT, width=32)
     val windowSize =  UInt(INPUT, width=32) // Might want to tweakerino
+    //val outChannels = UInt(INPUT, width=32)
     val addrImage =   UInt(INPUT, width=64)
     val addrResult =  UInt(INPUT, width=64)
+    //val addrWeights = UInt(INPUT, width=64)
     val start =       Bool(INPUT)
 
     // Outputs
     val finished = Bool(OUTPUT)
   }
-  
+
+  /** Returns an initialized reader
+    * @note memwriteport is plugged
+    * @note reader is default false
+    */  
   def initialize_reader(port: UInt) = {
     val reader = Module(new StreamReader(new StreamReaderParams(
       streamWidth = wordSize, fifoElems = 8, mem = p.toMemReqParams(),
@@ -38,14 +44,26 @@ class TestSlidingWindow(p: PlatformWrapperParams, _wordSize:Int) extends Generic
     reader.rsp <> io.memPort(port).memRdRsp
     reader
   }
-  
+
+  def initialize_writer (port: UInt) = {
+    val wr = Module(new StreamWriter(new StreamWriterParams(
+      streamWidth = p.memDataBits, mem = p.toMemReqParams(), chanID = 0
+    ))).io
+    wr.start := Bool(false)
+    wr.req <> io.memPort(port).memWrReq
+    wr.wdat <> io.memPort(port).memWrDat
+    io.memPort(port).memWrRsp <> wr.rsp
+    plugMemReadPort(port)
+    wr
+  }
+
   // Set initial state
   val s_idle :: s_read :: s_lowering :: Nil = Enum(UInt(), 3) 
   val state = Reg(init=UInt(s_idle))
   
   // Reader is default false
-  reader = initialize_reader(0) 
-
+  val reader = initialize_reader(0) 
+  val writer = initialize_writer(1)
   switch (state){
     is(s_idle){
       when (io.start){
@@ -53,11 +71,13 @@ class TestSlidingWindow(p: PlatformWrapperParams, _wordSize:Int) extends Generic
       }
     }
     is(s_read){
-      reader.start :=
-      reader.baseAddr :=
-      reader.byteCount :=
-
+      reader.baseAddr := AddrWeights
+      reader.byteCount := windowsize * numChannels
+      reader.start := Bool(true)
+      reader.out <> writer.in 
     }
-    is(s_lowering){}
+    is(s_lowering){
+     
+    }
   }
 }
