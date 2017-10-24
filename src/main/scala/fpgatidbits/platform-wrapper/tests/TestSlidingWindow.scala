d6e46cdbdca5116e18e6fd095ab070d2b13f6676
finninde <finninde@stud.ntnu.no>
2017-10-24 16:08:33 +0200

    Add io finished, and wrapper for state machine

--- src/main/scala/fpgatidbits/platform-wrapper/tests/TestSlidingWindow.scala
+++ src/main/scala/fpgatidbits/platform-wrapper/tests/TestSlidingWindow.scala
@@ -10,13 +10,25 @@ import fpgatidbits.rosetta._
 class TestSlidingWindow(p: PlatformWrapperParams, _wordSize:Int) extends GenericAccelerator(p) {
   val wordSize = _wordSize 
   val io = new GenericAcceleratorIF(numMemPorts, p){
-    val imageSizeX = UInt(INPUT, width=32)
-    val imageSizeY = UInt(INPUT, width=32)
+    val imageSizeX =  UInt(INPUT, width=32)
+    val imageSizeY =  UInt(INPUT, width=32)
     val numChannels = UInt(INPUT, width=32)
-    val stride = UInt(INPUT, width=32)
-    val windowSize = UInt(INPUT, width=32) // Might want to tweakerino
-    val addrImage = UInt(INPUT, width=64)
-    val addrResult = UInt(INPUT, width=64)
+    val stride =      UInt(INPUT, width=32)
+    val windowSize =  UInt(INPUT, width=32) // Might want to tweakerino
+    val addrImage =   UInt(INPUT, width=64)
+    val addrResult =  UInt(INPUT, width=64)
+    val start =       Bool(INPUT)
+
+    val finished = Bool(OUTPUT)
   }
+  val state = Reg(init=UInt(s_idle))
   
+  val s_idle :: s_read :: s_finished :: s_lowering :: Nil = Enum(UInt(), 4) 
+  
+  switch (state){
+    is(s_idle){}
+    is(s_read){}
+    is(s_lowering){}
+    is(s_finished){}
+  }
 }
