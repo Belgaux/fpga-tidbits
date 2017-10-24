1fdcc92b2392e7d106d6ecb3702756e5a53106e7
finninde <finninde@stud.ntnu.no>
2017-10-24 16:34:49 +0200

    Add initialization for reader as a class function Increases readability?

--- src/main/scala/fpgatidbits/platform-wrapper/tests/TestSlidingWindow.scala
+++ src/main/scala/fpgatidbits/platform-wrapper/tests/TestSlidingWindow.scala
@@ -9,7 +9,10 @@ import fpgatidbits.rosetta._
 
 class TestSlidingWindow(p: PlatformWrapperParams, _wordSize:Int) extends GenericAccelerator(p) {
   val wordSize = _wordSize 
+  val numMemPorts = 1
+
   val io = new GenericAcceleratorIF(numMemPorts, p){
+    // Inputs
     val imageSizeX =  UInt(INPUT, width=32)
     val imageSizeY =  UInt(INPUT, width=32)
     val numChannels = UInt(INPUT, width=32)
@@ -19,16 +22,42 @@ class TestSlidingWindow(p: PlatformWrapperParams, _wordSize:Int) extends Generic
     val addrResult =  UInt(INPUT, width=64)
     val start =       Bool(INPUT)
 
+    // Outputs
     val finished = Bool(OUTPUT)
   }
+  
+  def initialize_reader(port: UInt) = {
+    val reader = Module(new StreamReader(new StreamReaderParams(
+      streamWidth = wordSize, fifoElems = 8, mem = p.toMemReqParams(),
+      maxBeats = 1, chanID = 0, disableThrottle = true
+    ))).io
+    reader.out.ready := Bool(false)
+    reader.start := Bool(false) 
+    plugMemWritePort(port)
+    reader.req <> io.memPort(port).memRdReq 
+    reader.rsp <> io.memPort(port).memRdRsp
+    reader
+  }
+  
+  // Set initial state
+  val s_idle :: s_read :: s_lowering :: Nil = Enum(UInt(), 3) 
   val state = Reg(init=UInt(s_idle))
   
-  val s_idle :: s_read :: s_finished :: s_lowering :: Nil = Enum(UInt(), 4) 
-  
+  // Reader is default false
+  reader = initialize_reader(0) 
+
   switch (state){
-    is(s_idle){}
-    is(s_read){}
+    is(s_idle){
+      when (io.start){
+        state := s_read
+      }
+    }
+    is(s_read){
+      reader.start :=
+      reader.baseAddr :=
+      reader.byteCount :=
+
+    }
     is(s_lowering){}
-    is(s_finished){}
   }
 }
