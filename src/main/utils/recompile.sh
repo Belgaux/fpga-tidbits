738c1b68804fdc1fe20e17826a0af21fea39cb36
Håkon Flatval <hkon20@hotmail.com>
2017-10-29 14:07:38 +0100

    Make TestSlidingWindow work with some no. of channels
    Sliding window module now works with images whose channel count is a
    multiple of 8

--- src/main/utils/recompile.sh
+++ src/main/utils/recompile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ..
-sbt "run e TestBMVM Tester"
-cd emu-TestBMVM/
+sbt "run e TestSlidingWindow Tester"
+cd emu-TestSlidingWindow/
 make
-./app && cd .. && cd emu-TestBMVM
+./app && cd .. && cd emu-TestSlidingWindow
