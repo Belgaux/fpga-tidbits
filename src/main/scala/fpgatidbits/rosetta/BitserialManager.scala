f26fbc2add9a089febdd47709ea8413a19e31001
Belgaux <oscarbelgau@gmail.com>
2017-10-29 18:33:44 +0100

    Make state name more descriptive

--- src/main/scala/fpgatidbits/rosetta/BitserialManager.scala
+++ src/main/scala/fpgatidbits/rosetta/BitserialManager.scala
@@ -2,7 +2,7 @@ package fpgatidbits.rosetta
 import Chisel._
 
 class BitserialManager(word_size : Int, W_depth : Int, A_depth : Int) extends Module {
-  
+ 
   val io = new Bundle {
     val W = Decoupled(SInt(width = word_size)).flip
     val A = Decoupled(SInt(width = word_size)).flip
@@ -11,12 +11,12 @@ class BitserialManager(word_size : Int, W_depth : Int, A_depth : Int) extends Mo
     val out = SInt(OUTPUT, width = word_size)
   }
 
-  val s_idle :: s_accumulate :: s_pack :: s_compute :: s_done :: Nil = Enum(UInt(), 5)
+  val s_idle :: s_pop_elem :: s_pack :: s_compute :: s_done :: Nil = Enum(UInt(), 5)
   val state = Reg(init = UInt(s_idle))
   val tmp = Reg(init = SInt(0, width = word_size))
   io.out := tmp
 
-  val wsc = Reg(init = UInt(0, width = log2Up(word_size)))
+  val bitpack_count = Reg(init = UInt(0, width = log2Up(word_size)))
   val PE = Module(new Bitserial(word_size, W_depth, A_depth)).io
   PE.start     := Bool(false)
   PE.bitplane  := Bool(false)
@@ -30,15 +30,25 @@ class BitserialManager(word_size : Int, W_depth : Int, A_depth : Int) extends Mo
   switch (state) {
     
     is (s_idle) {
-      wsc := UInt(0)
+      bitpack_count := UInt(0)
       tmp := SInt(0)
-      when (io.start) { state := s_accumulate }
+      when (io.start) { state := s_pop_elem }
     }
-    
+ 
+    is (s_pop_elem) {
+      when (io.W.valid && io.A.valid) {
+        io.W.ready := Bool(true)
+        io.A.ready := Bool(true)
+        PE.start := Bool(true)  
+        PE.bitplane := Bool(true)
+        state := s_pack
+      }
+    }   
+
     is (s_pack) {
       when (PE.done) {
-        wsc := wsc + UInt(1)
-        state := Mux(wsc === UInt(word_size) - UInt(1), s_compute, s_accumulate)
+        bitpack_count := bitpack_count + UInt(1)
+        state := Mux(bitpack_count === UInt(word_size) - UInt(1), s_compute, s_pop_elem)
       } 
     }
 
@@ -53,21 +63,9 @@ class BitserialManager(word_size : Int, W_depth : Int, A_depth : Int) extends Mo
       }
     }
 
-    is (s_accumulate) {
-      when (io.W.valid && io.A.valid) {
-        io.W.ready := Bool(true)
-        io.A.ready := Bool(true)
-        PE.start := Bool(true)  
-        PE.bitplane := Bool(true)
-        state := s_pack
-      }
-    }
-
     is (s_done) {
       io.done := Bool(true)
       when (!io.start) { state := s_idle }
     }
-  
   }
-
 }
