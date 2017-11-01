2e6417ff8007c2b3c3f356d72209eee9f2e383f3
Håkon Flatval <hkon20@hotmail.com>
2017-11-01 21:58:10 +0100

    Try to implement BRAM cache for a start
    But something is terribly, terribly wrong. Everything gets written to
    the first position?

--- src/main/scala/fpgatidbits/platform-wrapper/tests/TestSlidingWindowBitplanes.scala
+++ src/main/scala/fpgatidbits/platform-wrapper/tests/TestSlidingWindowBitplanes.scala
@@ -3,17 +3,34 @@ package fpgatidbits.Testbenches
 import Chisel._
 
 import fpgatidbits.dma._
+import fpgatidbits.ocm._
 import fpgatidbits.streams._
 import fpgatidbits.PlatformWrapper._
 import fpgatidbits.rosetta._
 
+
+// Expects input of form channel/bits/rows/columns, with every row padded to wordsize bits
 class TestSlidingWindowBitplanes(p: PlatformWrapperParams, _wordSizeInBits:Int) extends GenericAccelerator(p) {
   val wordSizeInBits = _wordSizeInBits
   val wordSizeInBytes = wordSizeInBits/8
   val numMemPorts = 2
 
   val io = new GenericAcceleratorIF(numMemPorts, p){
+    val numCols = UInt(INPUT, width=16)
+    val numRows = UInt(INPUT, width=16)
+    val numBits = UInt(INPUT, width=16)
+    val numChannels = UInt(INPUT, width=16)
+    val windowSize = UInt(INPUT, width=16)
+    val stride = UInt(INPUT, width=16)
+    val addrImage =  UInt(INPUT, width=wordSizeInBits)
+    val addrResult = UInt(INPUT, width=wordSizeInBits)
+    val start = Bool(INPUT)
 
+    val finished = Bool(OUTPUT)
+
+    // Debug:
+    val checkAddrBRAM = UInt(INPUT, width=32)
+    val debugOutput = UInt(OUTPUT, width=wordSizeInBits)
   }
 
   /** Returns an initialized reader
@@ -45,21 +62,114 @@ class TestSlidingWindowBitplanes(p: PlatformWrapperParams, _wordSizeInBits:Int) 
   }
 
   // Set initial state
-  val s_idle :: s_read :: s_finished :: Nil = Enum(UInt(), 3) 
+  val s_idle :: s_slide :: s_fill_bram :: s_write_buffer :: s_finished :: Nil = Enum(UInt(), 5) 
   val state = Reg(init=UInt(s_idle))
 
   val reader = initialize_reader(0) 
   val writer = initialize_writer(1)
   reader.out <> writer.in
 
+  reader.baseAddr := UInt(0)
+  reader.byteCount := UInt(0)
+  reader.out.ready := Bool(false)
+
+  val bram = Module(new DualPortBRAM(addrBits = 32, dataBits = wordSizeInBits)).io
+
+  val bramWritePort = bram.ports(0)
+  val bramReadPort = bram.ports(1)
+
+  bramWritePort.req.addr := UInt(0)
+  bramWritePort.req.writeData := UInt(0)
+  bramWritePort.req.writeEn := Bool(false)
+
+  bramReadPort.req.writeEn := Bool(false)
+  bramReadPort.req.addr := UInt(0)
+  io.finished := Bool(false)
+
+  // Debug
+  //bramReadPort.req.addr := io.checkAddrBRAM
+  bramReadPort.req.addr := UInt(0)
+  io.debugOutput := bramReadPort.rsp.readData
+  printf("Debugoutput is %d read from %d\n", io.debugOutput, bramReadPort.req.addr)
+
+  val buffer = Reg(init=UInt(width=wordSizeInBits))
+
+  val currInputRow = UInt(width=16)
+  val currInputCol = UInt(width=16)
+  val currInputBitplane = UInt(width=16)
+  val currInputChannel = UInt(width=16)
+
+  val currOutputRow = UInt(width=32)
+  val currOutputBitplane = UInt(width=32)
+
+  val currInputFillingRow = Reg(init=UInt(0, width=16))
+  val currOutputFillingRow = Reg(init=UInt(0, width=16))
+  val currStartBRAMRow = Reg(init=UInt(0, width=16))
+
+  val wordBitExponent = log2Up(wordSizeInBits)
+  val wordByteExponent = wordBitExponent - 3
+  val inputBytesPerRow = (((io.numCols + UInt(wordSizeInBits - 1)) >> wordBitExponent) << wordByteExponent)
+  val inputBitplaneSize = Reg(init=UInt(0, width=16))
+  val inputChannelSize = Reg(init=UInt(0, width=32))
+  val inputBitsChannelsOffset = Reg(init=UInt(0, width=32))
+
+  val justFilledBRAM = Reg(init=Bool(false))
+
+  inputBitplaneSize := io.numRows * inputBytesPerRow
+  inputChannelSize := io.numBits * inputBitplaneSize
+  inputBitsChannelsOffset := inputChannelSize * currInputChannel + inputBitplaneSize * currInputBitplane
+
   switch (state){
     is(s_idle){
+      when(io.start){
+        state := s_fill_bram
+      }
     }
 
-    is(s_read){
+    is(s_slide){
+      when(((currInputCol & (UInt(wordSizeInBits - 1))) === UInt(0)) && !justFilledBRAM){
+        state := s_fill_bram
+        currOutputFillingRow := currStartBRAMRow
+        currInputFillingRow := UInt(0)
+      }.otherwise{
+        // Do some filling of buffer from BRAM
+        state := s_finished
+      }
     }
+
+    is(s_fill_bram){
+      reader.baseAddr := io.addrImage + inputBitsChannelsOffset + (currInputRow + currInputFillingRow) * inputBytesPerRow + (currInputCol >> (wordBitExponent - wordByteExponent))
+      reader.byteCount := UInt(wordSizeInBytes)
+      reader.start := Bool(true)
+      reader.out.ready := Bool(true)
+      bramWritePort.req.addr := currOutputFillingRow * inputBytesPerRow + (currInputCol >> wordBitExponent)
+      bramWritePort.req.writeEn := reader.out.valid
+      bramWritePort.req.writeData := reader.out.bits
+
+      when(reader.out.valid){
+
+        printf("Writing %b to address %d\n", bramWritePort.req.writeData, bramWritePort.req.addr)
+        reader.start := Bool(false)
+        when(currInputFillingRow === io.windowSize - UInt(1)){
+          justFilledBRAM := Bool(true)
+          state := s_slide
+        }.otherwise{
+          currInputFillingRow := currInputFillingRow + UInt(1)
+          when(currOutputFillingRow === io.windowSize - UInt(1)){
+            currOutputFillingRow := UInt(0)
+          }.otherwise{
+            currOutputFillingRow := currOutputFillingRow + UInt(1)
+          }
+        }
+      }
+    }
+
 
     is(s_finished){
+      io.finished := Bool(true)
+      when(~io.start){
+        state := s_idle
+      }
     }
   }
 }
